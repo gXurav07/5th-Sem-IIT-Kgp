32db2e008ce227e75f4a9154729917ab824bf4b0
Gaurav Malakar <varuag2002@gmail.com>
2022-08-16 23:31:00 +0530

    coa assign2

--- 5th_Sem/COA_Lab/Assignment-2/ass2_q3.s
+++ 5th_Sem/COA_Lab/Assignment-2/ass2_q3.s
@@ -1,17 +1,46 @@
 
         .globl main
         .data
+prompt:
+        .asciiz "Enter four positive integers m, n, a and r: "
+
 newline:
-    .asciiz "\n"
+        .asciiz "\n"
 space:
-    .asciiz " "
+        .asciiz " "
         .text
+
+# main program
+#
+# program variables
+#   m:   $s0
+#   n:   $s1
+#   a:   $s2
+#   r:   $s3
+#   A:   $s4
+#   B:   $s5
+#
 main:
 input:
-        li      $s0, 4  # m
-        li      $s1, 7  # n
-        li      $s2, 3  # $a
-        li      $s3, 2  # r
+        li      $v0, 4          # issue prompt
+        la      $a0, prompt
+        syscall
+
+        li      $v0, 5          # get m from user
+        syscall
+        move    $s0, $v0
+
+        li      $v0, 5          # get n from user
+        syscall
+        move    $s1, $v0
+
+        li      $v0, 5          # get a from user
+        syscall
+        move    $s2, $v0
+
+        li      $v0, 5          # get r from user
+        syscall
+        move    $s3, $v0
 
 initialize:
         jal     initStack
@@ -27,28 +56,29 @@ allocate_B:
         move    $s5, $v0        # $s5 = B
 
 populate_A:
+        move    $t7, $s2        # $t7 = $s2
         li      $t3, 0          # i=0
 
         
-for:    # for(i=0; i<n; i++)
-        bge     $t3, $s1, endfor 
-        mul     $t5, $t3, $s0   # t5 = i*m
+for:    # for(i=0; i<m; i++)
+        bge     $t3, $s0, endfor 
+        mul     $t5, $t3, $s1   # t5 = i*n
         li      $t4, 0          # j=0
-for2:   # for(j=0; j<m; j++)
-        bge     $t4, $s0, endfor2
-        add     $t6, $t5, $t4   # t6 = i*m + j
-        mul     $t6, $t6, 4     # t6 = 4*(i*m + j)
-        sub     $t6, $s4, $t6   # t6 = A - 4*(i*m + j)
+for2:   # for(j=0; j<n; j++)
+        bge     $t4, $s1, endfor2
+        add     $t6, $t5, $t4   # t6 = i*n + j
+        mul     $t6, $t6, 4     # t6 = 4*(i*n + j)
+        add     $t6, $s4, $t6   # t6 = A + 4*(i*n + j)
 
-        sw      $s2, ($t6)      # A[i][j] = $s2
-        mul     $s2,  $s2, $s3  # $s2 = $s2 * r
+        sw      $t7, ($t6)      # A[i][j] = $t7 ( currently $t7 has value a.r^(i*n+j) ) 
+        mul     $t7,  $t7, $s3  # $t7 = $t7 * r
 
-        addi    $t4, $t4, 1
-        b       for2      
+        addi    $t4, $t4, 1     # j = j + 1
+        b       for2            # continue loop
 
 endfor2:
-        addi    $t3, $t3, 1
-        b       for
+        addi    $t3, $t3, 1     # i = i + 1
+        b       for             # continue loop
 endfor:
 
 print_A:
@@ -93,8 +123,7 @@ pushToStack:
 mallocInStack:
         mul     $t0, $a0, 4
         sub     $sp, $sp, $t0
-        move    $v0, $sp
-        addi    $v0, 4          # address $sp is not a part of 4*$a0 sized 
+        move    $v0, $sp        
         jr      $ra
 
 
@@ -104,37 +133,37 @@ printMatrix:
 
         li      $t3, 0          # i=0
 
-for3:    # for(i=0; i<n; i++)
-        bge     $t3, $a1, endfor3 
-        mul     $t5, $t3, $t0   # $t5 = i*m
+for3:    # for(i=0; i<m; i++)
+        bge     $t3, $t0, endfor3 
+        mul     $t5, $t3, $a1   # $t5 = i*n
         li      $t4, 0          # j=0
-for4:   # for(j=0; j<m; j++)
-        bge     $t4, $t0, endfor4
-        add     $t6, $t5, $t4   # $t6 = i*m + j
-        mul     $t6, $t6, 4    # $t6 = 4*(i*m + j)
-        sub     $t6, $a2, $t6   # $t6 = Matrix - 4*(i*m + j)
+for4:   # for(j=0; j<n; j++)
+        bge     $t4, $a1, endfor4
+        add     $t6, $t5, $t4   # $t6 = i*n + j
+        mul     $t6, $t6, 4     # $t6 = 4*(i*n + j)
+        add     $t6, $a2, $t6   # $t6 = Matrix + 4*(i*n + j)
 
         lw      $a0, ($t6)      # $a0 = Matrix[i][j] 
-        li      $v0, 1
+        li      $v0, 1          # print Matrix[i][j]
         syscall
-        li      $v0, 4
-        la      $a0, space
+        li      $v0, 4          
+        la      $a0, space      # print space
         syscall
 
 
-        addi    $t4, $t4, 1
-        b       for4      
+        addi    $t4, $t4, 1     # j = j + 1
+        b       for4            # continue loop
 
 endfor4:
         li      $v0, 4
-        la      $a0, newline
+        la      $a0, newline    # print newline
         syscall
 
-        addi    $t3, $t3, 1
-        b       for3
+        addi    $t3, $t3, 1     # i = i + 1
+        b       for3            # continue loop
 endfor3:
         li      $v0, 4
-        la      $a0, newline
+        la      $a0, newline    # print newline
         syscall
         
         jr	$ra	
@@ -144,35 +173,35 @@ endfor3:
 transposeMatrix:
         li      $t3, 0          # i=0
 
-for5:    # for(i=0; i<n; i++)
-        bge     $t3, $a1, endfor5 
+for5:    # for(i=0; i<m; i++)
+        bge     $t3, $a0, endfor5 
         li      $t4, 0          # j=0
-for6:   # for(j=0; j<m; j++)
-        bge     $t4, $a0, endfor6
+for6:   # for(j=0; j<n; j++)
+        bge     $t4, $a1, endfor6
 
-        mul     $t5, $t3, $a0   # $t5 = i*m
-        add     $t6, $t5, $t4   # $t6 = i*m + j
-        mul     $t6, $t6, 4    # $t6 = 4*(i*m + j)
-        sub     $t6, $a2, $t6   # $t6 = A - 4*(i*m + j)
+        mul     $t5, $t3, $a1   # $t5 = i*n
+        add     $t6, $t5, $t4   # $t6 = i*n + j
+        mul     $t6, $t6, 4     # $t6 = 4*(i*n + j)
+        add     $t6, $a2, $t6   # $t6 = A + 4*(i*n + j)
 
         lw      $t0, ($t6)      # $t0 =  A[i][j]
 
-        mul     $t5, $t4, $a1   # $t5 = j*n
-        add     $t6, $t5, $t3   # $t6 = j*n + i
-        mul     $t6, $t6, 4    # $t6 = 4*(j*n + i)
-        sub     $t6, $a3, $t6   # $t6 = B - 4*(j*n + i)
+        mul     $t5, $t4, $a0   # $t5 = j*m
+        add     $t6, $t5, $t3   # $t6 = j*m + i
+        mul     $t6, $t6, 4     # $t6 = 4*(j*m + i)
+        add     $t6, $a3, $t6   # $t6 = B + 4*(j*m + i)
 
         sw      $t0, ($t6)      # B[j][i] = $t0 = A[i][j]
 
         
 
 
-        addi    $t4, $t4, 1     # j += 1
-        b       for6      
+        addi    $t4, $t4, 1     # j = j + 1
+        b       for6            # continue loop     
 
 endfor6:
-        addi    $t3, $t3, 1     # i += 1
-        b       for5
+        addi    $t3, $t3, 1     # i = i + 1
+        b       for5            # continue loop
 endfor5:
         jr	$ra
         
